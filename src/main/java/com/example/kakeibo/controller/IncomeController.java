9c35a0cce0f16c54acd85b18f89153f2395d553c
Yamada-Daichi <yamada.daichi@technopro.com>
2023-05-10 11:23:01 +0900

    incomeプッシュ

--- src/main/java/com/example/kakeibo/controller/IncomeController.java
+++ src/main/java/com/example/kakeibo/controller/IncomeController.java
@@ -2,32 +2,43 @@ package com.example.kakeibo.controller;
 
 
 
+import java.time.LocalDate;
+
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.kakeibo.model.Income;
-
-
-
+import com.example.kakeibo.repository.IncomeRepository;
+import com.example.kakeibo.service.IncomeService;
 
 @Controller
 public class IncomeController {
 	
+	@Autowired
+	private IncomeRepository incomeRepository;
+	
+//	@Autowired
+//	private IncomeService incomeService;
+	
 	//新規収入登録ページに遷移
 	@GetMapping("/newIncome")
-    public String getNewIncome() {
-		
+    public String getNewIncome(Model model, Income income) {
+		LocalDate nowDate = LocalDate.now();
+		model.addAttribute("nowDate", nowDate);
         return "newIncome";
     }
 	
 //	@PostMapping("/newIncome")
-//	public String createIncome() {
+//	public String createIncome(Income income) {
 //		
+//		incomeRepository.save(income);
 //		return "index2";
 //	}
 	
