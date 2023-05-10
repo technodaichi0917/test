035fec6b51199fc35fb9cbfee2807bd33b7c2145
Yamada-Daichi <yamada.daichi@technopro.com>
2023-05-10 17:21:10 +0900

    日付一覧取得

--- src/main/java/com/example/kakeibo/controller/TotalController.java
+++ src/main/java/com/example/kakeibo/controller/TotalController.java
@@ -1,32 +1,65 @@
 package com.example.kakeibo.controller;
 
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.kakeibo.model.Income;
+import com.example.kakeibo.model.Spending;
+import com.example.kakeibo.service.CalculateService;
+import com.example.kakeibo.service.IncomeService;
+import com.example.kakeibo.service.SpendingService;
 
 @Controller
 public class TotalController {
 	
 	
-	//今月の収入・支出の情報をそれぞれ一覧表示(金額、日付、メモ)
-	//今月の収入合計金額の表示
-	//今月の支出合計金額の表示
-	//今月の収支合計金額の表示
+	@Autowired
+	IncomeService incomeService;
 	
-	//今月の収支一覧画面へ遷移
+	@Autowired
+	SpendingService spendingService;
+	
+	@Autowired
+	CalculateService calculateService;
+	
 	@GetMapping("/currentMonthTotal")
-	public String getCurrentMonthTotal() {
+	public String getCurrentMonthTotal(Model model) {
+		LocalDate nowDate = LocalDate.now();
+		model.addAttribute("nowDate", nowDate);
+		List<Income> incomelist = incomeService.getCurrentIncomeList();
+		List<Spending> spendinglist = spendingService.getCurrentSpendingList();
+		int incomeTotal = calculateService.getCurrentIncomeTotal();
+		int spendingTotal = calculateService.getCurrentSpendingTotal();
+		int monthTotal = calculateService.getCurrentMonthTotal();
+		model.addAttribute("incomelist", incomelist);
+		model.addAttribute("spendinglist", spendinglist);
+		model.addAttribute("incomeTotal", incomeTotal);
+		model.addAttribute("spendingTotal", spendingTotal);
+		model.addAttribute("monthTotal", monthTotal);
 		return "currentMonthTotal";
 	}
 	
-	//別月の収入・支出の情報をそれぞれ一覧表示(金額、日付、メモ)
-	//別月の収入合計金額の表示
-	//別月の支出合計金額の表示
-	//別月の収支合計金額の表示
-	
-	//別月の収支一覧画面へ遷移
 	@GetMapping("/serchMonthTotal")
-	public String getOtherMonthTotal() {
+	public String getOtherMonthTotal(Model model, @RequestParam int year, @RequestParam int month) {
+//		
+//		List<Income> incomelist = incomeService.getIncomeList(year,month);
+//	    List<Spending> spendinglist = spendingService.getSpendingList(year,month);
+//	    
+//	    int incomeTotal = calculateService.getIncomeTotal(year,month);
+//	    int spendingTotal = calculateService.getSpendingTotal(year,month);
+//	    int monthTotal = calculateService.getMonthTotal();
+//	    model.addAttribute("incomelist", incomelist);
+//	    model.addAttribute("spendinglist", spendinglist);
+//	    model.addAttribute("incomeTotal", incomeTotal);
+//	    model.addAttribute("spendingTotal", spendingTotal);
+//	    model.addAttribute("monthTotal", monthTotal);
+		
 		return "serchMonthTotal";
 	}
-
 }
--- src/main/java/com/example/kakeibo/service/IncomeService.java
+++ src/main/java/com/example/kakeibo/service/IncomeService.java
@@ -1,7 +1,43 @@
 package com.example.kakeibo.service;
 
 
+import java.time.LocalDate;
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.kakeibo.model.Income;
+import com.example.kakeibo.repository.IncomeRepository;
+
+@Service
 public class IncomeService {
 	
-
+	@Autowired
+	private IncomeRepository incomeRepository;
+	
+	public List<Income> getCurrentIncomeList() {
+		LocalDate now = LocalDate.now();
+		LocalDate start = now.withDayOfMonth(1);
+		LocalDate end = now.withDayOfMonth(now.lengthOfMonth());
+		return incomeRepository.findAllByDatetimeBetween(start,end);
+	}
+	
+	public List<Income> getIncomeList(int year, int month){
+		LocalDate start = LocalDate.of(year, month,1);
+		LocalDate end = start.plusMonths(1).minusDays(1);
+		return incomeRepository.findAllByDatetimeBetween(start,end);
+	}
+	
+	@Transactional
+	public void updateIncome(Integer id, Integer incomeAmount, LocalDate datetime, String memo) {
+		
+		Income income = incomeRepository.findById(id).get();
+		incomeRepository.save(income);
+		
+	}
 }
+
+
